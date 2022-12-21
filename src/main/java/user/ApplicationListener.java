15c7f0d644ba87b97a9b297d0ecbd54d6256e296
Rodin Andrei <mail@arod.in>
2022-12-21 09:51:02 +0200

    Move DriverName to servlet context parameter

--- src/main/java/user/ApplicationListener.java
+++ src/main/java/user/ApplicationListener.java
@@ -6,10 +6,13 @@ import javax.servlet.ServletContextListener;
 public class ApplicationListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        String jdbcDriver = "org.postgresql.Driver";
+        String jdbcDriver = sce.getServletContext()
+                .getInitParameter("jdbcDriver");
+
         try {
             Class.forName(jdbcDriver);
         } catch (ClassNotFoundException e) {
+            System.out.println("Upps" + e.getMessage());
             throw new RuntimeException("Error load application. Cause: JDBC driver not found " + jdbcDriver);
         }
     }
