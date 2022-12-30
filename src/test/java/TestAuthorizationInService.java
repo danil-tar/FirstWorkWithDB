567895f66aaa999b1aacfc4a9fab771178c2f8c6
Dan <danilviktar@gmail.com>
2022-12-30 11:37:03 +0200

    Add doPost to AuthorizationService Add and mapping AuthorizationService to web.xml Add TestAuthorizationInService

---
+++ src/test/java/TestAuthorizationInService.java
@@ -0,0 +1,49 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import user.AuthorizationService;
+import user.User;
+import user.UserRepository;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestAuthorizationInService {
+
+//    @BeforeClass
+//   // @Test
+//    public static void createRowsToDataBaseForTest() throws SQLException {
+//
+//        UserRepository userRepository = new UserRepository();
+//        userRepository.getConnectionToDB();
+//        Statement statement = userRepository.getConnection().createStatement();
+//
+//        statement.execute("DELETE FROM users");
+//
+//        User user1 = new User(null, "dan", "dan@mail.ru", "123nj");
+//        User user2 = new User(null, "dan", "dan1253@mail.ru", "58rgg");
+//        User user3 = new User(null, "andi", "andi@mail.ru", "khjgyf14");
+//        User user4 = new User(null, "andi", "154andi@mail.ru", "pofu775mn");
+//        User user5 = new User(null, "Fan", "jhayt134@mail.ru", "iuf755f");
+//
+//        userRepository.createNewUser(user1);
+//        userRepository.createNewUser(user2);
+//        userRepository.createNewUser(user3);
+//        userRepository.createNewUser(user4);
+//        userRepository.createNewUser(user5);
+//
+//    }
+
+
+
+//    @AfterClass
+//    public static void clearAllRowsInDataBaseAfterTest() throws SQLException {
+//
+//        UserRepository userRepository = new UserRepository();
+//        userRepository.getConnectionToDB();
+//        Statement statement = userRepository.getConnection().createStatement();
+//
+//        statement.execute("DELETE FROM users");
+//
+//    }
+}
